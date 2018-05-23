dd3464c5f15bd8bcb03ada6072d64a317e2ef0bf
Cryptonex <hello@cryptonex.org>
2018-05-23 19:48:12 +0500

    add translation

--- src/translation/index.js
+++ src/translation/index.js
@@ -1,5 +1,24 @@
 import en from './en-UK';
+import ar from './ar';
+import ja from './ja';
+import es from './es';
+import de from './de';
+import fr from './fr';
+import hi from './hi';
+import it from './it';
+import ko from './ko';
+import ms from './ms';
+import nl from './nl';
+import pl from './pl';
+import vi from './vi';
+import zh from './zh';
+import ru from './ru';
+import tr from './tr';
+import pt from './pt';
 
 export default {
-  'en-UK': en,
+  ar, ja, es, de,
+  fr, hi, it, ko, ms,
+  nl, pl, vi, zh,
+  en, ru, tr, pt
 };
